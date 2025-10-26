ffd0081296e78649e61f777ffdb0b48c6bbf3306
yawara173946825 <yawara173946825@gmail.com>
2025-10-27 06:03:36 +0900

    add: reactをインストール

---
+++ laravel-blog/contents/blog/resources/js/reacgt-app.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Example from './Components/Example';
+
+createRoot(document.getElementById('react-app')).render(<Example />);
--- laravel-blog/contents/blog/vite.config.js
+++ laravel-blog/contents/blog/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react'; // 追加
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.jsx'], // app.js → app.jsx に変更
             refresh: true,
         }),
+        react(), // Reactプラグインを有効化
     ],
 });
